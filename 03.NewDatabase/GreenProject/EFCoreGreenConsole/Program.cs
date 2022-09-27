3340b61fba476d55fe7c1d056f0f8dfa38e77669
Ifeanyi Nwachukwu <ifenwachukwu7@outlook.com>
2022-09-27 18:23:43 +0100

    committing changes

---
+++ 03.NewDatabase/GreenProject/EFCoreGreenConsole/Program.cs
@@ -0,0 +1,68 @@
+﻿
+
+using EFCoreGreenConsole;
+using EFGreen_DbLibrary;
+using InventoryModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+Program1.BuildOptions();
+Program1.EnsureItems();
+Program1.ListInventory();
+//InventoryDbContext.DisplayConnectionString();
+public static class Program1
+{
+    private static IConfigurationRoot _configuration;
+
+    private static DbContextOptionsBuilder<InventoryDbContext> _optionsBuilder;
+
+   
+
+    public static void BuildOptions()
+    {
+        _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+        _optionsBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
+        _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("InventoryManager"));
+
+    }
+
+    public static void EnsureItem(string name)
+    {
+        using var db = new InventoryDbContext(_optionsBuilder.Options);
+        // determine if item exists:
+        var existigItem = db.Items.FirstOrDefault(x => x.Name.ToLower() == name.ToLower());
+
+        if (existigItem == null)
+        {
+            // doesn't exist, add it.
+
+            var item = new Item()
+            {
+                Name = name,
+            };
+
+            db.Items.Add(item);
+            db.SaveChanges();
+        }
+    }
+
+    public static void EnsureItems()
+    {
+        EnsureItem("Batman Begins");
+        EnsureItem("Inception");
+        EnsureItem("Remember the Titans");
+        EnsureItem("Star Wars: The Empire Strikes Back");
+        EnsureItem("Top Gun");
+    }
+
+    public static void ListInventory()
+    {
+        using(var db = new InventoryDbContext(_optionsBuilder.Options))
+        {
+            var items = db.Items.OrderBy(x => x.Name).ToList();
+
+            items.ForEach(x => Console.WriteLine($"New Item: {x.Name}"));
+        }
+    }
+}
---
+++ 03.NewDatabase/GreenProject/EFGreen_DbLibrary/EFGreen_DbLibrary/InventoryDbContext.cs
@@ -0,0 +1,57 @@
+﻿using InventoryModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
+namespace EFGreen_DbLibrary
+{
+    public class InventoryDbContext : DbContext
+    {
+        private static IConfigurationRoot _configuration;
+        public virtual DbSet<Item> Items { get; set; }
+        // Default constructor for when scaffolding is needed
+        public InventoryDbContext()
+        {
+
+        }
+        // Complex constructor for allowing Dependency Injection
+        public InventoryDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+                _configuration = builder.Build();
+                var cnstr = _configuration.GetConnectionString("InventoryManager");
+                optionsBuilder.UseSqlServer(cnstr);
+            }
+        }
+
+        //public static void DisplayConnectionString()
+        //{
+        //    var builder = new ConfigurationBuilder()
+        //                     .SetBasePath(Directory.GetCurrentDirectory())
+        //                     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+        //    _configuration = builder.Build();
+        //    var cnstr = _configuration.GetConnectionString("InventoryManager");
+
+        //    if(cnstr != null)
+        //    {
+        //        Console.WriteLine(cnstr);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("No connection string found");
+        //    }
+        //}
+    }
+}
